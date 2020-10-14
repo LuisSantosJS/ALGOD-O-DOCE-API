05bae75e22f693ead9a4edfbf0849d4e25b63d67
Luis Santos <dasilvasantosluisfelipe@gmail.com>
2020-10-14 11:13:28 -0500

    finish

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -51,7 +51,7 @@ module.exports = {
                 });
             }
             const token = jwt.sign({ email }, process.env.SECRET || 'issosecreto', {
-                expiresIn: 7200 // expires in 120min
+               
             });
             return response.json({ message: 'success', token: token, res: 'Usuário logado com sucesso' })
         });
